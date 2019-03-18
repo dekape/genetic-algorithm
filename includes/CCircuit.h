99e8907d2d6148bb6dce4a9d7167eb83c7af3c46
Duncan-Hunter <dunchunter@hotmail.co.uk>
2019-03-18 15:53:55 +0000

    initial validity check

--- includes/CCircuit.h
+++ includes/CCircuit.h
@@ -1,5 +1,5 @@
 const int num_units = 10;
 
-bool Check_Validity(int *circuit_vector);
+bool Check_Validity(CUnit *units);
 
 void mark_units(int unit_num);
--- src/CCircuit.cpp
+++ src/CCircuit.cpp
@@ -4,36 +4,82 @@
 #include <CUnit.h>
 #include <CCircuit.h>
 
-std::vector<CUnit> units(num_units);
+//std::vector<CUnit> units(num_units);
+int no_units = 2;
 
-bool Check_Validity(int *circuit_vector)
+void unitsToIntArray(int *IntArray, CUnit &units_to_convert)
 {
+  //IntArray is where to store answer
+  IntArray[0] = 0;
 
+  
+
+}
+
+bool checkValidity(CUnit *units, int no_units)
+{
+  bool conc_exit(false), tail_exit(false);
+
+  markUnits(0, units, conc_exit, tail_exit); //units[0].id
+  //if not traversed to exit
+  if(!conc_exit && !tail_exit)
+    {
+      return false;
+    }
+  
+  for(int i = 0; i < no_units; i++)
+  {
+    //if exits go to same place
+    if(units[i].conc_num == units[i].tails_num)
+    {
+      return false;
+    }
+    //if conc goes to itself, tails goes to itself
+    if(units[i].conc_num == i || units[i].tails_num == i)
+    {
+      return false;
+    }
+    //if not traversed at all
+    if(!units[i].mark)
+    {
+      return false;
+    }
+  }
   return true;
 }
 
-void mark_units(int unit_num) {
+//CUnit * units
+void markUnits(int unit_num, CUnit *units, bool &conc_exit, bool &tail_exit) {
 
+  //if marked, dont bother
   if (units[unit_num].mark) return;
-
+  //mark it as visited
   units[unit_num].mark = true;
 
   //If we have seen this unit already exit
   //Mark that we have now seen the unit
   //If conc_num does not point at a circuit outlet recursively call the function
 
+  //if its next unit is not an exit, mark it
   if (units[unit_num].conc_num<num_units) {
-    mark_units(units[unit_num].conc_num);
+    markUnits(units[unit_num].conc_num, units, conc_exit, tail_exit); //go to 
   } else {
+    conc_exit = true;
     // ...Potentially do something to indicate that you have seen an exit
   }
   //If tails_num does not point at a circuit outlet recursively call the function 
 
+  //if its next unit is not an exit, mark it
   if (units[unit_num].tails_num<num_units) {
-    mark_units(units[unit_num].tails_num); 
+    markUnits(units[unit_num].tails_num, units, conc_exit, tail_exit); 
   } else {
+    tail_exit = true;
     // ...Potentially do something to indicate that you have seen an exit
   }
 }
+//CUnit* 
+
+
+
 
 
