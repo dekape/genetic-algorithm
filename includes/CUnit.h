488aa05419527459fa3ef7838c24ad7ac754b28c
Hitesh Kumar <hiteshkumar9@live.co.uk>
2019-03-18 17:39:32 +0000

    Added stream class and updated stream attributes of unit class

--- includes/CUnit.h
+++ includes/CUnit.h
@@ -1,16 +1,45 @@
+class CStream {
+public:
+
+
+  // Proportions - how much of this stream's mass is
+  // concentrate vs tail
+	double concentrate;
+  double tail;
+
+
+  // Operator overloads to help calculations later
+	CStream operator+(CStream &rhs);
+	CStream &operator+=(CStream &rhs);
+
+
+};
+
+
+
 class CUnit {
- public:
-  //index of the unit to which this unit’s concentrate stream is connected 
+  public:
+
+
+  // Indexes to which this unit's output streams are connected 
   int conc_num;
-  //index of the unit to which this unit’s concentrate stream is connected 
   int tails_num;
-  //A Boolean that is changed to true if the unit has been seen 
+  // Mark unit as seen or not seen
   bool mark;
+
+
+  // Streams for the total input (old and new)
+  // and the two outputs (concentrate and tail)
+  CStream current_input_feed, old_input_feed, 
+          conc_output, tail_output;
+
 
   /*
 
     ...other member functions and variables of CUnit
 
   */
+
+
 };
 
