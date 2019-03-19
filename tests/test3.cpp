24545de6f25dc1c67a67551adfb69e024ca9b053
Hitesh Kumar <hiteshkumar9@live.co.uk>
2019-03-19 15:31:38 +0000

    Tests for circuit modelling suite written

---
+++ tests/test3.cpp
@@ -0,0 +1,57 @@
+#include <cmath>
+#include <iostream>
+
+#include "CUnit.h"
+#include "CCircuit.h"
+#include "Genetic_Algorithm.h"
+
+
+using namespace std;
+
+
+int main(int argc, char * argv[])
+{
+
+
+    // Intitialise test circuit as array of ints
+    // Provided in lecture slides 
+    int num_units = 5;
+    int actual_fitness = 24.82;
+    int test_array[11] = {0, 4, 3, 2, 0, 5, 4, 4, 6, 2, 1};
+
+    
+    // Create unit array from int array of circuit
+    CUnit test_circuit[5];
+    intArrayToUnits(test_array, test_circuit, num_units);
+
+
+    // Check if our test case is valid
+    bool valid_test = checkValidity(test_array, test_circuit, num_units);
+    if (!valid_test) {
+        cout << "Invalid test.\n";
+        cout << "fail\n";
+    }
+
+    
+    // Finally convert to vector of units
+    vector<CUnit> circuit_vector(num_units);
+    unitArrayToVector(test_circuit, circuit_vector, num_units);
+
+    
+    // Balance masses of circuit and get final outputs
+    double fitness;
+    fitness = balance_mass(circuit_vector, 10e-8);  
+
+
+    // See if the difference is within tolerance
+    double difference = fitness - actual_fitness;
+    if (difference < 10e-6) {
+        std::cout  << "pass\n";
+	} 
+    else
+    {
+	    std::cout << "fail\n";
+    }
+    
+
+}
