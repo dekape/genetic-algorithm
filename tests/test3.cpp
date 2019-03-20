adf08522a7bfafa956ea0376e70c0cf0590d4751
Hitesh Kumar <hiteshkumar9@live.co.uk>
2019-03-20 13:39:40 +0000

    updated new test to fit with the working balance mass function

--- tests/test3.cpp
+++ tests/test3.cpp
@@ -13,46 +13,55 @@ int main(int argc, char * argv[])
 {
 
 
-    // Intitialise test circuit as array of ints
-    // Provided in lecture slides 
-    int num_units = 5;
-    double actual_fitness = 24.82;
-    int test_array[11] = {0, 4, 3, 2, 0, 5, 4, 4, 6, 2, 1};
+	// Intitialise test circuit as array of ints
+	// Provided in lecture slides 
+	int num_units = 5;
+	double actual_fitness = 24.82;
+	int * test_array = new int[11];
+	test_array[0] = 0; test_array[1] = 4; test_array[2] = 3;
+	test_array[3] = 2; test_array[4] = 0; test_array[5] = 5;
+	test_array[6] = 4; test_array[7] = 4; test_array[8] = 6;
+	test_array[9] = 2; test_array[10] = 1;
 
-    
-    // Create unit array from int array of circuit
-    /*CUnit test_circuit[5];
-    intArrayToUnits(test_array, test_circuit, num_units);*/
+
+	// Create unit array from int array of circuit
+	CUnit test_circuit[5];
+	//intArrayToUnits(test_array, test_circuit, num_units);
+
+	CCircuit cc(5, test_array);
 
 
-    //// Check if our test case is valid
-    //bool valid_test = checkValidity(test_array, test_circuit, num_units);
-    //if (!valid_test) {
-    //    cout << "Invalid test.\n";
-    //    cout << "fail\n";
-    //}
+	//// Check if our test case is valid
+	//bool valid_test = checkValidity(cc);
+	//if (!valid_test) {
+	//	cout << "Invalid test.\n";
+	//	cout << "fail\n";
+	//}
 
-    
-    // Finally create the circuit object
-	CCircuit circuit_obj(num_units, test_array);
 
-    
-    // Balance masses of circuit and get final outputs
-    double fitness;
-    fitness = balance_mass(circuit_obj, 10e-8);  
-	cout << fitness << endl;
+	// Finally convert to vector of units
+	vector<CUnit> circuit_vector(num_units);
+	unitArrayToVector(test_circuit, circuit_vector, num_units);
 
-    // See if the difference is within tolerance
-    double difference = fitness - actual_fitness;
+
+	// Balance masses of circuit and get final outputs
+	double fitness;
+	fitness = balance_mass(cc, 10e-8);
+
+
+	// See if the difference is within tolerance
+	double difference = fitness - actual_fitness;
+	cout << "The finess result is : " << fitness << endl;
+
 	printf("\n");
-    if (abs(difference) < 10e-6) {
-        std::cout  << "pass\n";
-	} 
-    else
-    {
-	    std::cout << "fail\n";
-    }
-    
+	if (abs(difference) < 10e-2) {
+		std::cout << "pass\n";
+	}
+	else
+	{
+		std::cout << "fail\n";
+	}
+
 
 	system("pause");
 
