d0fe6caa5bd69410fccc410513f3147d915a965d
czp21c <zongpengchen@gmail.com>
2019-03-20 15:12:12 +0000

    Optimisation of pairparents

--- Some functions.cpp
+++ Some functions.cpp
@@ -172,3 +172,71 @@ void checkTermination(int iter_count, int** offspring, int convergence_limit)
 			count++
 		} while (count < iter_count);
 }
+
+//Optimisation part
+//Optimized function in case of dividing the pairing and generating apart.
+//After calculating fitness each iterations.(Add it in main)
+
+double totalFitness = 0; //total fitness after fix
+
+int min_fit = 0; //compensation number to make sure all fitness numbers are larger than 0
+for (int i = 0; i < no_circuits; i++)
+{
+	if (fitness[i] < min_fit)
+	{
+		min_fit = fitness[i];
+	}
+}
+
+for (int i = 0; i < no_circuits; i++) //fix the fitness array and calculate the totalFitness (whether to just change the fitness array or just make a new array?)
+{
+	fitness[i] -= min_fit;
+	totalFitness += fitness[i];
+}
+
+//Change it in GA.cpp
+void pairParents(int ** circuits, int * parentA, int * parentB, int no_units, int no_circuits, double* fitness, double totalFitness)
+{
+	int index1 = -1; //index for parentA
+	int index2 = -1; //index for parentB
+
+	double ref1 = ((double)rand()) / RAND_MAX; //get a random number within the range of totalFitness
+	double refNum1 = ref1 * totalFitness;
+	double fitnessRef = 0; //use a reference number to get the index
+
+	for (int i = 0; i < no_circuits; i++) //get the index from the random number
+	{
+		fitnessRef += fitness[i];
+		if (fitnessRef >= refNum1)
+		{
+			index1 = i;
+			break;
+		}
+	}
+	fitnessRef = 0; //clear the fitness reference
+
+	do
+	{
+		double ref2 = ((double)rand()) / RAND_MAX; //get another random number
+		double refNum2 = ref2 * totalFitness;
+
+		for (int i = 1; i < no_circuits; i++)
+		{
+			fitnessRef += fitness[i];
+			if (fitnessRef >= refNum2)
+			{
+				if (index1 != i) //make sure it's not equal to parentA
+				{
+					index2 = i;
+				}
+				break; //if it is equal, break anyways
+			}
+		}
+	} while (index2 == -1);
+
+	for (int i = 0; i < 2 * no_units + 1; i++) //assign it to the pointer of parentA and B
+	{
+		parentA[i] = circuits[index1][i];
+		parentB[i] = circuits[index2][i];
+	}
+}
